53c9789c801be7adf1a918b98b5234206d4811d2
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-06-08 12:09:17 +0300

    start

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={}></ImageBackground>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
