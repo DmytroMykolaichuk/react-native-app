12876d71665582e4ab3a4dd534e23bf5e577dbdc
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-07-12 12:48:18 +0300

    test fb

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,14 +9,13 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-n
 // import { API_KEY } from 'react-native-dotenv';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD1LR2yh-MFZYy1ON3ZSwptG1vVnreDhm4',
-  authDomain: 'react-native-app-ec253.firebaseapp.com',
-  projectId: 'react-native-app-ec253',
-  storageBucket: 'react-native-app-ec253.appspot.com',
-  messagingSenderId: '73035882958',
-  appId: '1:73035882958:web:991601951f13b632394b9f',
+  apiKey: 'AIzaSyBCGzfsT7KvNWdX3-vMkPsilb9qcmt6GY4',
+  authDomain: 'app-native-react.firebaseapp.com',
+  projectId: 'app-native-react',
+  storageBucket: 'app-native-react.appspot.com',
+  messagingSenderId: '185236211549',
+  appId: '1:185236211549:web:249def3de93c0140748ab4',
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
